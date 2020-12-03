0764ee08a30a7a120bfa6e01b7ee66c8a3c16644
Diego <sturbadiego25@outlook.it>
2020-12-03 17:01:49 +0100

    Milestone 2 step 3

--- js/script.js
+++ js/script.js
@@ -17,6 +17,20 @@ https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&
 crubs
 */
 
+/*
+Milestone 3:
+In questa milestone come prima cosa aggiungiamo la copertina del film o della serie
+al nostro elenco. Ci viene passata dall’API solo la parte finale dell’URL, questo
+perché poi potremo generare da quella porzione di URL tante dimensioni diverse.
+Dovremo prendere quindi l’URL base delle immagini di TMDB:
+https://image.tmdb.org/t/p/ per poi aggiungere la dimensione che vogliamo generare
+(troviamo tutte le dimensioni possibili a questo link:
+https://www.themoviedb.org/talk/53c11d4ec3a3684cf4006400 ) per poi aggiungere la
+parte finale dell’URL passata dall’API.
+Esempio di URL che torna la copertina di BORIS:
+https://image.tmdb.org/t/p/w185/s2VDcsMh9ZhjFUxw77uCFDpTuXp.jpg
+*/
+
 const filmAPI = 'https://api.themoviedb.org/3/search/movie?api_key=e5c4638bfc15f5da668dafcb6d0b4b1b&query=';
 const serieTvAPI = 'https://api.themoviedb.org/3/search/tv?api_key=e5c4638bfc15f5da668dafcb6d0b4b1b&query=';
 
@@ -37,13 +51,28 @@ var app = new Vue ({
         risposta.data.results.forEach(item => {
           Vue.set(item, 'vote_average', Math.ceil(item.vote_average / 2));
         });
-        this.film = [...risposta.data.results]
+        this.film = [...risposta.data.results];
       });
       // API serie TV da chiamare
-      axios.get()
+      axios.get(serieTvAPI + this.search)
       .then(rispostaTV => {
-
+        let dataTv = rispostaTV.data.results;
+        this.search = '';
+        let serieTvUniformate = dataTv.map(item => {
+          item.original_title = item.original_name;
+          item.title = item.name;
+          return item;
+          console.log(serieTvUniformate);
+        });
+        this.film = [...this.film, ...serieTvUniformate];
       })
     }
   }
 })
+
+/*
+//li metto dentro this.film ciclando
+serieTvUniformate.forEach(item => {
+this.film.push(item)
+})
+*/
