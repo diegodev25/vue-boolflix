c7beef80db9dbd87f0c89684ade6da0c49d31f36
Diego <sturbadiego25@outlook.it>
2020-12-03 15:12:39 +0100

    Mileston 2 step 1

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 /*
-Milestone 1:
-Creare un layout base con una searchbar (una input e un button) in cui possiamo
-scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il
-bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni
-film trovato:
-1. Titolo
-2. Titolo Originale
-3. Lingua
-4. Voto
+Milestone 2:
+Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da
+permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5,
+lasciando le restanti vuote (troviamo le icone in FontAwesome).
+Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze
+piene (o mezze vuote :P)
+Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della
+nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della
+nazione ritornata dall’API (le flag non ci sono in FontAwesome).
+Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca
+dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando
+attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di
+risposta diversi, simili ma non sempre identici)
+Qui un esempio di chiamata per le serie tv:
+https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=s
+crubs
 */
 
 const personalAPI = 'https://api.themoviedb.org/3/search/movie?api_key=e5c4638bfc15f5da668dafcb6d0b4b1b&query=';
@@ -17,13 +23,20 @@ var app = new Vue ({
   el: '#app',
   data: {
     film: [],
-    search: ''
+    search: '',
+    stars: 5
   },
   methods: {
     checkFilm: function () {
       axios.get(personalAPI + this.search)
-      .then(risposta => this.film = risposta.data.results);
-      this.search = '';
+      .then(risposta => {
+        this.film = risposta.data.results;
+        this.search = '';
+        risposta.data.results.forEach(item => {
+          Vue.set(item, 'vote_average', Math.ceil(item.vote_average / 2));
+        });
+        this.film = [...risposta.data.results]
+      })
     }
   }
 })
