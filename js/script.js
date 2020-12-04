3d3c812f05f3d8638859cc925638ab329822d42e
Diego <sturbadiego25@outlook.it>
2020-12-04 15:48:16 +0100

    Milestone 4 step 1

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
 /*
-Milestone 3:
-In questa milestone come prima cosa aggiungiamo la copertina del film o della serie
-al nostro elenco. Ci viene passata dall’API solo la parte finale dell’URL, questo
-perché poi potremo generare da quella porzione di URL tante dimensioni diverse.
-Dovremo prendere quindi l’URL base delle immagini di TMDB:
-https://image.tmdb.org/t/p/ per poi aggiungere la dimensione che vogliamo generare
-(troviamo tutte le dimensioni possibili a questo link:
-https://www.themoviedb.org/talk/53c11d4ec3a3684cf4006400 ) per poi aggiungere la
-parte finale dell’URL passata dall’API.
-Esempio di URL che torna la copertina di BORIS:
-https://image.tmdb.org/t/p/w185/s2VDcsMh9ZhjFUxw77uCFDpTuXp.jpg
+Milestone 4:
+Trasformiamo quello che abbiamo fatto fino ad ora in una vera e propria webapp,
+creando un layout completo simil-Netflix:
+● Un header che contiene logo e search bar
+● Dopo aver ricercato qualcosa nella searchbar, i risultati appaiono sotto forma
+di “card” in cui lo sfondo è rappresentato dall’immagine di copertina ( consiglio
+la poster_path con w342 )
+● Andando con il mouse sopra una card (on hover), appaiono le informazioni
+aggiuntive già prese nei punti precedenti più la overview
 */
 
 const filmAPI = 'https://api.themoviedb.org/3/search/movie?api_key=e5c4638bfc15f5da668dafcb6d0b4b1b&query=';
@@ -19,34 +17,36 @@ var app = new Vue ({
   el: '#app',
   data: {
     film: [],
-    search: '',
-    stars: 5
+    query: ''
   },
   methods: {
     checkFilm: function () {
       // API film da chiamare
-      axios.get(filmAPI + this.search)
+      axios.get(filmAPI + this.query)
       .then(risposta => {
         this.film = risposta.data.results;
-        this.search = '';
+        this.query = '';
         risposta.data.results.forEach(item => {
           Vue.set(item, 'vote_average', Math.ceil(item.vote_average / 2));
         });
         this.film = [...risposta.data.results];
       });
       // API serie TV da chiamare
-      axios.get(serieTvAPI + this.search)
+      axios.get(serieTvAPI + this.query)
       .then(rispostaTV => {
         let dataTv = rispostaTV.data.results;
-        this.search = '';
+        this.query = '';
         let serieTvUniformate = dataTv.map(item => {
           item.original_title = item.original_name;
           item.title = item.name;
           return item;
         });
-        console.log(serieTvUniformate);
         this.film = [...this.film, ...serieTvUniformate];
-      })
+      });
+    },
+    //funzione per inserire un poster di default nel caso mancasse la copertina
+    posterDefault(event) {
+      event.target.src = "img/poster-random.jpg";
     }
   }
 })
