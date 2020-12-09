6897a33185372c6db410a4606c20b1fef3f4410e
Diego <sturbadiego25@outlook.it>
2020-12-09 09:16:22 +0100

    Refactoring html css Boolflix

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 /*
-Milestone 4:
-Trasformiamo quello che abbiamo fatto fino ad ora in una vera e propria webapp,
-creando un layout completo simil-Netflix:
-● Un header che contiene logo e search bar
-● Dopo aver ricercato qualcosa nella searchbar, i risultati appaiono sotto forma
-di “card” in cui lo sfondo è rappresentato dall’immagine di copertina ( consiglio
-la poster_path con w342 )
-● Andando con il mouse sopra una card (on hover), appaiono le informazioni
-aggiuntive già prese nei punti precedenti più la overview
+Milestone 5 (Opzionale):
+Partendo da un film o da una serie, richiedere all'API quali sono gli attori che fanno
+parte del cast aggiungendo alla nostra scheda Film / Serie SOLO i primi 5 restituiti
+dall’API con Nome e Cognome, e i generi associati al film con questo schema:
+“Genere 1, Genere 2, …”.
+Milestone 6 (Opzionale):
+Creare una lista di generi richiedendo quelli disponibili all'API e creare dei filtri con i
+generi tv e movie per mostrare/nascondere le schede ottenute con la ricerca.
 */
 
 const filmAPI = 'https://api.themoviedb.org/3/search/movie?api_key=e5c4638bfc15f5da668dafcb6d0b4b1b&query=';
@@ -37,6 +36,7 @@ var app = new Vue ({
         let dataTv = rispostaTV.data.results;
         this.query = '';
         let serieTvUniformate = dataTv.map(item => {
+          Vue.set(item, 'vote_average', Math.ceil(item.vote_average / 2));
           item.original_title = item.original_name;
           item.title = item.name;
           return item;
